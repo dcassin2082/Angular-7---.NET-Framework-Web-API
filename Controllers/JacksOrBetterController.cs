c51ec040da7ec08e006ad0fa86cd286fbab094a0
dcassin2082 <dcassin2082@gmail.com>
2019-09-04 15:58:17 -0700

    Add files via upload

---
+++ Controllers/JacksOrBetterController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebApi.Models;
+using WebApi.Services;
+
+namespace WebApi.Controllers
+{
+    [RoutePrefix("api/jacksorbetter")]
+    public class JacksOrBetterController : ApiController
+    {
+        private IJacksOrBetterService _jacksOrBetterService;
+
+        public JacksOrBetterController(IJacksOrBetterService gameService)
+        {
+            _jacksOrBetterService = gameService;
+        }
+
+        [Route("paytable/{username}")]
+        [HttpGet]
+        public GameModel GetPaytable([FromUri] string username)
+        {
+            return _jacksOrBetterService.GetPaytable(username);
+        }
+
+        [Route("deal")]
+        [HttpGet]
+        public GameModel Deal(int bet, string username)
+        {
+            return _jacksOrBetterService.Deal(bet, username);
+        }
+
+        [Route("draw/{arr}")]
+        [HttpGet]
+        public GameModel Draw([FromUri]int[] arr, int bet, string username)
+        {
+            return _jacksOrBetterService.Draw(arr, bet, username);
+        }
+    }
+}
---
+++ Controllers/JokersWildController.cs
@@ -0,0 +1,38 @@
+﻿using System.Web.Http;
+using WebApi.Models;
+using WebApi.Services;
+
+namespace WebApi.Controllers
+{
+    [RoutePrefix("api/jokerswild")]
+    public class JokersWildController : ApiController
+    {
+        private IJokersWildService _gameService;
+
+        public JokersWildController(IJokersWildService gameService)
+        {
+            _gameService = gameService;
+        }
+
+        [Route("paytable/{username}")]
+        [HttpGet]
+        public GameModel GetPaytable([FromUri] string username)
+        {
+            return _gameService.GetPaytable(username);
+        }
+
+        [Route("deal")]
+        [HttpGet]
+        public GameModel Deal(int bet, string username)
+        {
+            return _gameService.Deal(bet, username);
+        }
+
+        [Route("draw/{arr}")]
+        [HttpGet]
+        public GameModel Draw([FromUri]int[] arr, int bet, string username)
+        {
+            return _gameService.Draw(arr, bet, username);
+        }
+    }
+}
