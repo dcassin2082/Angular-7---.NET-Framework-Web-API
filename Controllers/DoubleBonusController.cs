0174e0258ead1e5ca1e926101aae276e2a150b72
dcassin2082 <dcassin2082@gmail.com>
2019-09-04 15:57:45 -0700

    Add files via upload

---
+++ Controllers/DoubleBonusController.cs
@@ -0,0 +1,38 @@
+﻿using System.Web.Http;
+using WebApi.Models;
+using WebApi.Services;
+
+namespace WebApi.Controllers
+{
+    [RoutePrefix("api/doublebonus")]
+    public class DoubleBonusController : ApiController
+    {
+        private IDoubleBonusService _doubleBonusService;
+
+        public DoubleBonusController(IDoubleBonusService gameService)
+        {
+            _doubleBonusService = gameService;
+        }
+
+        [Route("paytable/{username}")]
+        [HttpGet]
+        public GameModel GetPaytable([FromUri] string username)
+        {
+            return _doubleBonusService.GetPaytable(username);
+        }
+
+        [Route("deal")]
+        [HttpGet]
+        public GameModel Deal(int bet, string username)
+        {
+            return _doubleBonusService.Deal(bet, username);
+        }
+
+        [Route("draw/{arr}")]
+        [HttpGet]
+        public GameModel Draw([FromUri]int[] arr, int bet, string username)
+        {
+            return _doubleBonusService.Draw(arr, bet, username);
+        }
+    }
+}
