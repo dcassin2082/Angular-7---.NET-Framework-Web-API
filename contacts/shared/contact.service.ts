b8e54e0738400017e1ba8fc12fbc779795c4b101
dcassin2082 <dcassin2082@gmail.com>
2019-09-05 11:38:23 -0700

    Update contact.service.ts

--- contacts/shared/contact.service.ts
+++ contacts/shared/contact.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Contact } from './contact.model';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
@@ -9,11 +10,18 @@ export class ContactService {
 
   constructor(private http: HttpClient) { }
 
-  baseUrl = "https://localhost:44331/api/contacts";
+  baseUrl: string;
   contact: Contact;
   contacts: Contact[];
   regions: string[] = ["East", "Midwest", "Central", "North", "Northeast", "Northwest", "South", "Southeast", "Southwest", "West"]
+  
   getContacts() {
+    if(environment.production){
+      this.baseUrl = "http://dcassin5938-001-site1.ctempurl.com/api/contacts";
+    }
+    else{
+      this.baseUrl = "https://localhost:44331/api/contacts"
+    }
     this.http.get(this.baseUrl).toPromise().then(x => {
       this.contacts = x as Contact[];
     })
